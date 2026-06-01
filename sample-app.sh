033c484c2a8eaf73ea73c2b34e37ab075ffc3525
lortesnyder <ben.reyesh@duocuc.cl>
2026-06-01 00:27:04 +0000

    Docker y CI/CD

--- sample-app.sh
+++ sample-app.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
-set -e
 
-rm -rf tempdir
-mkdir -p tempdir/templates
-mkdir -p tempdir/static
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
 
-cp sample_app.py tempdir/
-cp -r templates/* tempdir/templates/
-cp -r static/* tempdir/static/
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
 
-cat > tempdir/Dockerfile <<'EOF'
-FROM python:3.10-slim
-
-WORKDIR /home/myapp
-
-COPY ./static /home/myapp/static/
-COPY ./templates /home/myapp/templates/
-COPY sample_app.py /home/myapp/
-
-RUN python -m pip install --no-cache-dir --progress-bar off flask
-
-EXPOSE 9999
-
-CMD ["python", "/home/devasc/labs/devnet-src/jenkins/sample-app/sample_app.py"]
-EOF
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 9999" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
-
-docker rm -f samplerunning || true
-docker build --no-cache -t sampleapp .
-docker run -d -p 9999:9999 --name samplerunning sampleapp python /home/devasc/labs/devnet-src/jenkins/sample-app/sample_app.py
-docker ps -a
+docker build -t sampleapp .
+docker run -t -d -p 9999:9999 --name samplerunning sampleapp
+docker ps -a 
